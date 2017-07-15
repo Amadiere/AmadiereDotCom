5b7d3ee13ce20517624283361c33f5bd87e5a266
Amadiere <amadiere@amadiere.com>
2017-07-15 11:13:26 +0100

    Added some sharing buttons

--- Website/ViewModels/Blog/ArticleViewModel.cs
+++ Website/ViewModels/Blog/ArticleViewModel.cs
@@ -1,4 +1,6 @@
-﻿namespace Amadiere.Website.ViewModels.Blog
+﻿using System.Net;
+
+namespace Amadiere.Website.ViewModels.Blog
 {
     public class ArticleViewModel
     {
@@ -7,6 +9,26 @@
         public string PublishedOn
         {
             get { return Article?.PublishedOn?.ToString("F"); }
+        }
+
+        public string FullUrl
+        {
+            get { return $"http://amadiere.com/blog/{Article.Year}/{Article.Month}/{Article.Slug}"; }
+        }
+
+        public string EncodedTitle
+        {
+            get { return WebUtility.UrlEncode(Article.Title); }
+        }
+
+        public string EncodedDescription
+        {
+            get { return WebUtility.UrlEncode(Article.Description); }
+        }
+
+        public string EncodedHattip
+        {
+            get { return WebUtility.UrlEncode($"Hi, Thought this article entitled I found at Amadiere.com would be interesting."); }
         }
     }
 }
